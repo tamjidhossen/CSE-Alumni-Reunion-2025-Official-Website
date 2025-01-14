5d1bb014c4616472b722280139cc1197b8de2ac0
Nabeel Ahsan <nabeel.cse15.jkkniu@gmail.com>
2025-01-15 01:27:32 +0600

    Annoucement api updated

--- Backend/routes/announcement.routes.js
+++ Backend/routes/announcement.routes.js
@@ -4,5 +4,8 @@ const upload = require('../utils/upload');
 const announcementController = require('../controllers/announcement.controller.js');
 const adminAuth = require('../middleware/adminAuth.middleware.js')
 router.post('/add', adminAuth.authAdmin, announcementController.createAnnouncement);
+router.get('/get-announcement', announcementController.getAnnouncements);
+router.get('/update/:id', adminAuth.authAdmin, announcementController.updateAnnouncement);
+router.delete('/delete/:id', adminAuth.authAdmin, announcementController.deleteAnnouncement);
 
 module.exports = router;
